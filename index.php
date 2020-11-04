f8a6926f55ab2fe2bcf41368cd4fb33303610cf4
PhuNT <phunt@neoscorp.vn>
2020-11-04 11:42:45 +0700

    Test landing page

--- index.php
+++ index.php
@@ -8,6 +8,9 @@
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 <style>
 html,body,h1,h2,h3,h4,h5,h6 {font-family: "Roboto", sans-serif}
+.w3-text-black, .w3-hover-text-black:hover {
+    margin-top:5px!important;
+}
 </style>
 <body class="w3-light-grey">
 
